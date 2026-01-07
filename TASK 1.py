bd4aa41d46a32c8c180b804458e29a59f6ed0061
Tanishkabadhai <176281113+Tanishkabadhai@users.noreply.github.com>
2026-01-07 11:54:19 +0530

    Add Task 1: multilevel inheritance with data hiding in Python

---
+++ TASK 1.py
@@ -0,0 +1,21 @@
+class Mobile:
+   def __init__(self,mobile_name,version,battery_power,price):
+    self.mobile_name=mobile_name
+    self.version=version
+    self.battery_power=battery_power
+    self.__price=price
+class Tecno(Mobile):
+    def __init__(self,mobile_name,version,battery_power,price):
+      super().__init__(mobile_name,version,battery_power,price)
+           
+class Samsung(Tecno):
+    def __init__(self, mobile_name, version, battery_power, price):
+        super().__init__(mobile_name, version, battery_power, price)
+
+    def display_details(self):
+        print("Mobile Name:", self.mobile_name)
+        print("Version:", self.version)
+        print("Battery Power:", self.battery_power)
+        print("Price: Confidential")
+phone = Samsung("Galaxy", "S23", "5000mAh", 70000)
+phone.display_details()
